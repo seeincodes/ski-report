a3d044700da369b67d5743ade4f5bce2a5188f5e
Xian Zheng <seeinplay@gmail.com>
2023-12-23 12:04:55 -0700

    empty slate

---
+++ src/app/types.tsx
@@ -0,0 +1,108 @@
+import { Chip } from "@mui/material";
+
+export function SnowflakeIcon() {
+  return (
+    <Chip
+      icon={
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='24'
+          height='24'
+          viewBox='0 0 24 24'
+          fill='none'
+          stroke='currentColor'
+          strokeWidth='2'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        >
+          <line x1='2' x2='22' y1='12' y2='12' />
+          <line x1='12' x2='12' y1='2' y2='22' />
+          <path d='m20 16-4-4 4-4' />
+          <path d='m4 8 4 4-4 4' />
+          <path d='m16 4-4 4-4-4' />
+          <path d='m8 20 4-4 4 4' />
+        </svg>
+      }
+    />
+  );
+}
+
+export function CloudIcon() {
+  return (
+    <Chip
+      icon={
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='24'
+          height='24'
+          viewBox='0 0 24 24'
+          fill='none'
+          stroke='currentColor'
+          strokeWidth='2'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        >
+          <path d='M17.4 19c1.8-2 2-5.1 0-7.1s-5-2.1-7 0c0 0-1 0-1 0a4.8 4.8 0 0 0-5 5c0 3 2 5 5 5h8c2 0 3-1 3-3s-1-2-3-3z' />
+        </svg>
+      }
+    />
+  );
+}
+
+export function SunIcon() {
+  return (
+    <Chip
+      icon={
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='24'
+          height='24'
+          viewBox='0 0 24 24'
+          fill='none'
+          stroke='currentColor'
+          strokeWidth='2'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        >
+          <circle cx='12' cy='12' r='5' />
+          <line x1='12' y1='1' x2='12' y2='3' />
+          <line x1='12' y1='21' x2='12' y2='23' />
+          <line x1='4.22' y1='4.22' x2='5.64' y2='5.64' />
+          <line x1='18.36' y1='18.36' x2='19.78' y2='19.78' />
+          <line x1='1' y1='12' x2='3' y2='12' />
+          <line x1='21' y1='12' x2='23' y2='12' />
+          <line x1='4.22' y1='19.78' x2='5.64' y2='18.36' />
+          <line x1='18.36' y1='5.64' x2='19.78' y2='4.22' />
+        </svg>
+      }
+    />
+  );
+}
+
+export function CloudSnowIcon() {
+  return (
+    <Chip
+      icon={
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='24'
+          height='24'
+          viewBox='0 0 24 24'
+          fill='none'
+          stroke='currentColor'
+          strokeWidth='2'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        >
+          <path d='M20 17.6a4 4 0 0 0-3.4-6.6c0 0-1 0-1 0a5 5 0 0 0-9 3 5 5 0 0 0 1 10h11a4 4 0 0 0 3.4-6.4z' />
+          <line x1='8' y1='16' x2='8.01' y2='16' />
+          <line x1='8' y1='20' x2='8.01' y2='20' />
+          <line x1='12' y1='18' x2='12.01' y2='18' />
+          <line x1='12' y1='22' x2='12.01' y2='22' />
+          <line x1='16' y1='16' x2='16.01' y2='16' />
+          <line x1='16' y1='20' x2='16.01' y2='20' />
+        </svg>
+      }
+    />
+  );
+}
