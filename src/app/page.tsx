90093e5e4b5e7ddb8fe2bf06db1b866487b6c7fd
Xian Zheng <seeinplay@gmail.com>
2023-12-23 14:24:49 -0700

    change to call openweathermap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,62 @@
+"use client";
 import Link from "next/link";
 import styles from "./page.module.css";
 import { SnowflakeIcon } from "./types";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const resorts = [
+  const [resorts, setResorts] = useState([
     {
       name: "Winter Park",
       temp: "25°F",
       snowfall: '8"',
+      lat: 39.8868,
+      long: -105.7625,
       website: "https://www.winterparkresort.com/",
     },
     {
       name: "Copper Mountain",
       temp: "23°F",
       snowfall: '6"',
+      lat: 39.502,
+      long: -106.1511,
       website: "https://www.coppercolorado.com/",
     },
     {
       name: "Steamboat Springs",
       temp: "28°F",
       snowfall: '10"',
+      lat: 40.485,
+      long: -106.8317,
       website: "https://www.steamboat.com/",
     },
     {
       name: "Arapahoe Basin",
       temp: "22°F",
       snowfall: '7"',
+      lat: 39.6426,
+      long: -105.8719,
       website: "https://www.arapahoebasin.com/",
     },
-  ];
+  ]);
 
-  async function getWeather(location: string) {
-    return fetch(
-      `http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${location}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        return data;
-      });
+  async function getWeather() {
+    for (let resort of resorts) {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/3.0/onecall?lat=${resort.lat}&lon=${resort.long}&appid=${process.env.NEXT_PUBLIC_WEATHER_API}`
+      );
+      const data = await response.json();
+      // Update the temperature and snowfall in the resort object
+      console.log("Weather Data", data);
+    }
+    // Update the state with the new resorts data
+    setResorts([...resorts]);
   }
+
+  useEffect(() => {
+    const weather = getWeather();
+    console.log(weather);
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -53,7 +71,7 @@ export default function Home() {
               <SnowflakeIcon />
             </div>
             <div className={styles.cardContent}>
-              <p>Temperature: {resort.temp}</p>
+              <p>Temp: {resort.temp}</p>
               <p>Snowfall: {resort.snowfall}</p>
               <Link target='_blank' href={resort.website} passHref>
                 Visit Site
